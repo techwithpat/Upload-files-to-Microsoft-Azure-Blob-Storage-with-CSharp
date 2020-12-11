315bef681dba341e38a9433a3a1ee28fd27920a5
Tshibanda Patrick <tshibandap@gmail.com>
2020-12-12 00:52:57 +0100

    first commit

---
+++ AzureBlobStorage/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureBlobStorage
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var config = GetConfiguration();
+
+            var files = GetFiles(config["AzureStorage:SourceFolder"]);                          
+            if (!files.Any())
+            {
+                Console.WriteLine("Nothing to process");
+                return;
+            }
+
+            UploadFiles(files, config["AzureStorage:ConnectionString"], config["AzureStorage:Container"]);
+        }
+
+        static IConfigurationRoot GetConfiguration()
+            => new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        static IEnumerable<FileInfo> GetFiles(string sourceFolder)
+            => new DirectoryInfo(sourceFolder)
+                .GetFiles()
+                .Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden));
+        
+        static void UploadFiles(
+            IEnumerable<FileInfo> files,
+            string connectionString,
+            string container)
+        {            
+            var containerClient = new BlobContainerClient(connectionString, container);
+
+            Console.WriteLine("Uploading files to blob storage");
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    var blobClient = containerClient.GetBlobClient(file.Name);
+                    using (var fileStream = File.OpenRead(file.FullName))
+                    {
+                        blobClient.Upload(fileStream);
+                    }
+
+                    Console.WriteLine($"{file.Name} uploaded");
+
+                    File.Delete(file.FullName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }        
+    }
+}
---
+++ AzureBlobStorage/README.md
@@ -0,0 +1,9 @@
+﻿#Upload files to Microsoft Azure Blob Storage
+
+In this tutorial, you will learn how to upload files to Microsoft Azure Blob Storage.
+You are going to create a console application with C# and upload files to blob storage.
+
+To run this solution, fill-in the configuration settings in appsettings.json.
+
+The step by step video is available on YouTube :
+[https://www.youtube.com/watch?v=JZWaWAU548g&lc](https://www.youtube.com/watch?v=JZWaWAU548g&lc)
